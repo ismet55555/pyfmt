0bd7d1b73318ff80c7012c7dd175da7a69c205fb
Ismet <ismet55555@users.noreply.github.com>
2019-11-01 14:25:02 -0400

    Update pyfmt/__init__.py
    Co-Authored-By: Dustin Rohde <xdrohdex@gmail.com>

--- pyfmt/__init__.py
+++ pyfmt/__init__.py
@@ -127,7 +127,7 @@ def pyfmt(
         display_divider(title="SKIPPING FILES")
         for filename_to_skip in filenames_to_skip:
             print(f"SKIPPING: {filename_to_skip}")
-        print("\nNumber of files to be skipped: {}".format(len(filenames_to_skip)))
+        print(f"\nNumber of files to be skipped: {len(filenames_to_skip)}")
         # Make a continuos string of arguments for
         #   isort - must be separate --skip for each file
         #   black - regex for exact filename (ie. file1|file2|etc.)
